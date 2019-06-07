036c72fbef0628e99bafed6cc69361095c7091f3
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-06-07 11:09:50 +0900

    add delet

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -11,8 +11,22 @@ class SpacesController < ApplicationController
   	  redirect_to new_space_path
 	end
 
+	def show
+		@space = target_space params[:id]
+	end
+
+	def destroy
+	  @space = target_space params[:id]
+	  @space.destroy
+	  redirect_to new_space_path
+	end
+
 	private
 	def space_params
       params.require(:space).permit(:name, :room, :r_at)
+    end
+
+    def target_space space_id
+      current_user.spaces.where(id: space_id).take
     end
 end
