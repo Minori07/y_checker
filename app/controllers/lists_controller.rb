56b87cb19a98134f4d00695d0ba9a4ba1e00362b
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-12-17 23:07:52 +0900

    back up 12/17

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,88 @@
 class ListsController < ApplicationController
   before_action :authenticate_user!
+
+ #  def index
+ #  	@lists = current_user.lists
+ #  	@spaces = current_user.spaces
+ #  	@body = []
+ #  	last = @lists[@lists.length-1]
+ #  	reset = @lists[@lists.length-(@spaces.length+1)]
+ #  	l_y = last.c_at.year
+ #  	l_m = last.c_at.month
+ #  	l_d = last.c_at.day
+ #  	msg = "test,#{last.name},#{l_y}/#{l_m}/#{l_d} #{last.c_at.strftime("%H:%M:%S")},#{last.room},\n"
+ #  	r_y = reset.c_at.year
+ #  	r_m = reset.c_at.month
+ #  	r_d = reset.c_at.day
+ #  	msg2 = "test,#{reset.name},#{r_y}/#{r_m}/#{r_d} #{reset.c_at.strftime("%H:%M:%S")},#{reset.room},\n"
+ #  	same = 0
+ #  	last_n = @lists[@lists.length-1].name
+ #  	last_a = @lists[@lists.length-(@spaces.length+1)].c_at
+ #  	last_s = @lists[@lists.length-1].switch
+ #  	last_r = @lists[@lists.length-1].room
+
+ #  	require 'net/imap'
+	# require 'kconv'
+	# require 'mail'
+
+	# # imapに接続
+	# imap_host = 'imap.gmail.com' # imapをgmailのhostに設定する
+	# imap_usessl = true # imapのsslを有効にする
+	# imap_port = 993 # ssl有効なら993、そうでなければ143
+	# imap = Net::IMAP.new(imap_host, imap_port, imap_usessl)
+	# # imapにログイン
+	# imap_user = 'minotaka.0730@gmail.com'
+	# imap_passwd = 'dzzaypxjafwutmtw'
+	# imap.login(imap_user, imap_passwd)
+
+	# imap.select('INBOX') # 対象のメールボックスを選択
+	# ids = imap.search(['ALL']) # 全てのメールを取得
+
+	# imap.fetch(ids, "RFC822").each do |mail|
+	#   m = Mail.new(mail.attr["RFC822"])
+	#   # multipartなメールかチェック
+	#   if m.multipart?
+	#     # plantextなメールかチェック
+	#     if m.text_part
+	#     	if last.switch == 1
+	# 	    	if m.text_part.decoded.to_s == msg
+	# 	    		same = 1
+	# 	    	end
+	# 	    	if same == 1 && m.text_part.decoded.to_s != msg
+	# 	    		@body.push(m.text_part.decoded.to_s)
+	# 	    	end
+	#     	elsif last.switch == 0
+	#     		if m.text_part.decoded.to_s == msg2
+	# 	    		same = 1
+	# 	    	end
+	# 	    	if same == 1 && m.text_part.decoded.to_s != msg2
+	# 	    		@body.push(m.text_part.decoded.to_s)
+	# 	    	end
+	# 	    end
+
+	#     # htmlなメールかチェック
+	#     elsif m.html_par
+	#       body = m.html_part.decoded
+	#     end
+	#   else
+	#     body = m.body
+	#   end
+	# end
+
+	# @body.each do |body|
+	# 	@b = body.split(",")
+	# 	list = List.new
+	# 	list = current_user.lists.new
+	# 	list.name = @b[1]
+	# 	list.switch = 1
+	# 	list.c_at = Time.parse(@b[2])
+	# 	list.c_day = Date.parse(@b[2])
+	# 	list.room = @b[3].to_i
+	# 	list.save!
+	# end
+
+ #  end
+
   def index
   	@lists = current_user.lists
   	@spaces = current_user.spaces
@@ -68,7 +151,6 @@ class ListsController < ApplicationController
 	
 
   end
-  
   def room
   	@spaces = target_space params[:room]
   	@lists = target_room params[:room]
